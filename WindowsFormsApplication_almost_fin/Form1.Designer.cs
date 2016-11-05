229aaab52539df4cfc3c360370fe9943e0c88b8d
crepuscularseal <crepuscularseal@DESKTOP-P2MS8C5>
2016-11-05 16:26:16 +0800

    加入專案檔案。

---
+++ WindowsFormsApplication_almost_fin/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace WindowsFormsApplication_almost_fin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.vertical_now = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.horizon_now = new System.Windows.Forms.Label();
+            this.var_z_trackBar1 = new System.Windows.Forms.TrackBar();
+            this.var_x_trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.var_z_trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.var_x_trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(538, 298);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(45, 336);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(538, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(45, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(364, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Camera_start/stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.radioButton9);
+            this.groupBox1.Controls.Add(this.radioButton8);
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(45, 412);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(538, 106);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "XY_PLAN_CONTROL";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(406, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(335, 76);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(29, 16);
+            this.radioButton9.TabIndex = 8;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "3";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(202, 76);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(29, 16);
+            this.radioButton8.TabIndex = 7;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "2";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(71, 76);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(29, 16);
+            this.radioButton7.TabIndex = 6;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "1";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(335, 54);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(29, 16);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(202, 54);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(29, 16);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(71, 54);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(29, 16);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(335, 32);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(29, 16);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "9";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(202, 32);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(29, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "8";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(71, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(29, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "7";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(617, 435);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "-255上浮";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(617, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "255下潛";
+            // 
+            // vertical_now
+            // 
+            this.vertical_now.AutoSize = true;
+            this.vertical_now.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.vertical_now.Location = new System.Drawing.Point(602, 461);
+            this.vertical_now.Name = "vertical_now";
+            this.vertical_now.Size = new System.Drawing.Size(62, 16);
+            this.vertical_now.TabIndex = 14;
+            this.vertical_now.Text = "vertical=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(747, 435);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(735, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "255";
+            // 
+            // horizon_now
+            // 
+            this.horizon_now.AutoSize = true;
+            this.horizon_now.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.horizon_now.Location = new System.Drawing.Point(706, 461);
+            this.horizon_now.Name = "horizon_now";
+            this.horizon_now.Size = new System.Drawing.Size(64, 16);
+            this.horizon_now.TabIndex = 17;
+            this.horizon_now.Text = "horizon=";
+            // 
+            // var_z_trackBar1
+            // 
+            this.var_z_trackBar1.Location = new System.Drawing.Point(619, 42);
+            this.var_z_trackBar1.Maximum = 255;
+            this.var_z_trackBar1.Minimum = -255;
+            this.var_z_trackBar1.Name = "var_z_trackBar1";
+            this.var_z_trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.var_z_trackBar1.Size = new System.Drawing.Size(45, 390);
+            this.var_z_trackBar1.TabIndex = 18;
+            this.var_z_trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // var_x_trackBar2
+            // 
+            this.var_x_trackBar2.Location = new System.Drawing.Point(725, 42);
+            this.var_x_trackBar2.Maximum = 255;
+            this.var_x_trackBar2.Name = "var_x_trackBar2";
+            this.var_x_trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.var_x_trackBar2.Size = new System.Drawing.Size(45, 390);
+            this.var_x_trackBar2.TabIndex = 19;
+            this.var_x_trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(415, 362);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 31);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "&Take_photo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(603, 489);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(64, 28);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "歸零";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 539);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.var_x_trackBar2);
+            this.Controls.Add(this.var_z_trackBar1);
+            this.Controls.Add(this.horizon_now);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.vertical_now);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "ROV_UI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.var_z_trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.var_x_trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label vertical_now;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label horizon_now;
+        private System.Windows.Forms.TrackBar var_z_trackBar1;
+        private System.Windows.Forms.TrackBar var_x_trackBar2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication_almost_fin/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+using System.IO.Ports;
+
+using System.Diagnostics;
+
+namespace WindowsFormsApplication_almost_fin
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        string xy_solar;  //xy平面的運動形式
+        string x_power_handle; //x向推力的持續
+        string z_power_handle; //z向推力的持續
+
+        SerialPort port;
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+       
+         //打開PORT，並測試，為PORT溝通之物
+        private void init()
+        {
+            port = new SerialPort();
+            port.PortName = "COM3";
+            port.BaudRate = 9600;
+            
+            try 
+            {
+                port.Open();
+            }
+            catch(Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+        }
+
+        //外框載入，選擇視訊鏡頭輸入源
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo device in videoDevices)
+            {
+                comboBox1.Items.Add(device.Name); 
+            }
+            comboBox1.SelectedIndex = 0;
+            
+            videoSource = new VideoCaptureDevice();
+        }
+
+        //確定哪個為我們要的視訊鏡頭輸入源
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+            }
+            else 
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
+                //設定新的 frame event handler
+                videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+                videoSource.Start();
+            }
+        }
+
+        //訊號源框架，將載入物設為點陣圖檔
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = image;
+        }
+
+        //關閉圖源的時機
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource.IsRunning)
+                videoSource.Stop();
+            port.WriteLine("5,0,0");
+        }
+        
+        
+        //以下為控制xy_solar的，即控制xy平面運動模式的程式--------是為向量
+
+        //BTU 2 為按下一個RADIO 按鍵後，再按下BTU2 輸出訊息 
+        //XY平面的第一種操控法-滑鼠選擇
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Get_xy_dir(radioButton1);
+            Get_xy_dir(radioButton2);
+            Get_xy_dir(radioButton3);
+            Get_xy_dir(radioButton4);
+            Get_xy_dir(radioButton5);
+            Get_xy_dir(radioButton6);
+            Get_xy_dir(radioButton7);
+            Get_xy_dir(radioButton8);
+            Get_xy_dir(radioButton9);
+        }
+
+        private void Get_xy_dir(RadioButton rdobutton)
+        {
+            if (rdobutton.Checked)
+            {
+                // MessageBox.Show(rdobutton.Text.ToString());
+                //port.WriteLine(rdobutton.ToString());
+                xy_solar = rdobutton.Text.ToString();
+                port.WriteLine(xy_solar + "," + x_power_handle + "," + z_power_handle);
+            }
+        }
+
+        //XY平面的第二種控法-鍵盤
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                iniarbt();
+                radioButton2.Checked = true;
+                Get_xy_dir(radioButton2);
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                iniarbt();
+                radioButton4.Checked = true;
+                Get_xy_dir(radioButton4);
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                iniarbt();
+                radioButton8.Checked = true;
+                Get_xy_dir(radioButton8);
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                iniarbt();
+                radioButton6.Checked = true;
+                Get_xy_dir(radioButton6);
+            }
+            
+            //左後和右後
+            if (e.KeyCode == Keys.Z)
+            {
+                iniarbt();
+                radioButton7.Checked = true;
+                Get_xy_dir(radioButton7);
+            }
+            if (e.KeyCode == Keys.X)
+            {
+                iniarbt();
+                radioButton9.Checked = true;
+                Get_xy_dir(radioButton9);
+            }
+            //左後和右後
+            if (e.KeyCode == Keys.Q)
+            {
+                iniarbt();
+                radioButton1.Checked = true;
+                Get_xy_dir(radioButton1);
+            }
+            if (e.KeyCode == Keys.E)
+            {
+                iniarbt();
+                radioButton3.Checked = true;
+                Get_xy_dir(radioButton3);
+            }
+
+
+            //水平停機
+            if (e.KeyCode == Keys.P)
+            {
+                iniarbt();
+                radioButton5.Checked = true;
+                Get_xy_dir(radioButton5);
+            }
+        }
+
+        //將全盤先淨空
+        private void iniarbt()
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            radioButton5.Checked = false;
+            radioButton6.Checked = false;
+            radioButton7.Checked = false;
+            radioButton8.Checked = false;
+            radioButton9.Checked = false;
+        }
+
+        //深淺推力的量值
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if(port.IsOpen)
+            {
+                z_power_handle = var_z_trackBar1.Value.ToString();
+                port.WriteLine(xy_solar+","+x_power_handle+","+ z_power_handle);
+                vertical_now.Text = "vertical =" + var_z_trackBar1.Value.ToString();
+             }
+        }
+
+        //用CHECK BOX 和鍵盤的852處理定力
+
+
+        
+        //水平推力的量值
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            if (port.IsOpen)
+            {
+                x_power_handle  = var_x_trackBar2.Value.ToString();
+                port.WriteLine(xy_solar + "," + x_power_handle + "," + z_power_handle);
+                horizon_now.Text = "horizon =" + var_x_trackBar2.Value.ToString();
+            }
+        }
+
+
+        //攝影程式呼叫的按鍵
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\Users\user\Desktop\RCControl\Remote Camera Control");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var_z_trackBar1.Value = 0;
+            z_power_handle = var_z_trackBar1.Value.ToString();
+            port.WriteLine(xy_solar + "," + x_power_handle + "," + z_power_handle);
+            vertical_now.Text = "vertical =" + var_z_trackBar1.Value.ToString();
+
+        }
+    }
+}
